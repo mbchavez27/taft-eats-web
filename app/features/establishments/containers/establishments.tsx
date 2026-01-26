14f08cce9fa507c2c423501b6df6a40d9781a073
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2026-01-27 00:24:30 +0800

    feat: temporary add the carousel with the todo of fixing the width issues

--- app/features/establishments/containers/establishments.tsx
+++ app/features/establishments/containers/establishments.tsx
@@ -10,7 +10,7 @@ export default function Establishments() {
             ESTABLISHMENTS
           </h1>
         </header>
-        <ScrollArea className="h-120 w-[60%]">
+        <ScrollArea className="h-120">
           <section className="flex flex-wrap gap-2 md:gap-12 lg:gap-8 justify-start">
             <EstablishmentsCard name="La Tocaaaaaaaaaaaaaaa" rating={3} />
             <EstablishmentsCard name="La Toca" />
