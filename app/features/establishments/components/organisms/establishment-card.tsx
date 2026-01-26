893390e7adbe9842f21a6c6fba58cf23dac92f1d
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2026-01-26 16:26:21 +0800

    feat: fixed truncation

--- app/features/establishments/components/organisms/establishment-card.tsx
+++ app/features/establishments/components/organisms/establishment-card.tsx
@@ -10,13 +10,13 @@ export default function EstablishmentsCard({
   return (
     <>
       <main className="flex flex-col gap-2">
-        <section className="bg-[#FFD24D] w-42 h-42 rounded-md flex flex-col justify-between p-2 gap-4 drop-shadow-2xl">
+        <section className="bg-[#FFD24D] w-44 min-h-44 rounded-md flex flex-col justify-between p-2 gap-4 drop-shadow-2xl">
           <div className="flex justify-center">
             <div className="bg-[#D9D9D9] w-full h-24 rounded-md px-1 py-2">
               <div className="bg-[#848484] w-7 h-7 rounded-full"></div>
             </div>
           </div>
-          <div className="font-lexend font-bold text-[#326F33] text-xl pb-2 text-wrap">
+          <div className="font-lexend font-bold text-[#326F33] text-xl pb-2 truncate">
             {name ?? 'Establishment Name'}
           </div>
         </section>
