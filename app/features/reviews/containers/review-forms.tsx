ea58182b2b18d78b589df44eb352c7cc869741a9
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2026-01-28 16:28:21 +0900

    feat: finish review forms

--- app/features/reviews/containers/review-forms.tsx
+++ app/features/reviews/containers/review-forms.tsx
@@ -3,12 +3,26 @@ import Stars from "~/features/shared/components/molecules/stars";
 export default function ReviewForms() {
   return (
     <>
-      <main className="bg-white rounded-xl w-full p-5 px-5 py-3 sm:py-8 sm:px-10 flex flex-col gap-4 h-125 overflow-hidden font-lexend font-bold text-[#326F33]">
+      <main className="bg-white rounded-xl w-full p-5 sm:py-8 sm:px-10 flex flex-col gap-10 h-125 overflow-hidden font-lexend font-bold text-[#326F33]">
         <section className="flex flex-col gap-5">
           <h1 className="text-3xl">How would you rate your experience?</h1>
           <div className="flex gap-4 px-2">
             <Stars size={48} />
           </div>
+        </section>
+        <section className="flex flex-col gap-5 flex-1">
+          <label htmlFor="review" className="text-2xl">
+            Tell us about your experience
+          </label>
+          <textarea
+            name="review"
+            id="review"
+            className="text-black text-md border-2 border-black outline-none focus:outline-none rounded-lg p-3 w-full h-full resize-none"
+            placeholder="Write your review here..."
+          ></textarea>
+          <p className="text-[#9CB16F]">
+            Reviews need to be at least 20 characters.
+          </p>
         </section>
       </main>
     </>
