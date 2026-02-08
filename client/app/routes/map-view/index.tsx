a96aaf33dc5fbeefbbcc7c4791d7bd0296fb0734
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2026-02-09 01:15:14 +0800

    feat: add map view

--- client/app/routes/map-view/index.tsx
+++ client/app/routes/map-view/index.tsx
@@ -1,7 +1,6 @@
-import FilterMenu from '~/features/filter-menu/containers/filter-menu'
+import { Suspense, lazy } from 'react'
 import type { Route } from './+types/index'
-import FoodFilter from '~/features/food-filter/containers/food-filter'
-import EstablishmentsLists from '~/features/establishments/containers/establishments-lists'
+const MapView = lazy(() => import('~/features/map/containers/MapView'))
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: 'Taft Eats' }, { name: 'description', content: 'Taft Eats' }]
@@ -14,7 +13,17 @@ export default function Home() {
       <div className="hidden lg:block lg:w-1/4"></div>
 
       {/* Main content */}
-      <div className="w-full lg:w-3/4 flex flex-col gap-6 md:gap-8"></div>
+      <div className="w-full lg:w-3/4 flex flex-col gap-6 md:gap-8">
+        <Suspense
+          fallback={
+            <div className="text-white font-climate text-2xl animate-pulse">
+              Loading...
+            </div>
+          }
+        >
+          <MapView />
+        </Suspense>
+      </div>
     </main>
   )
 }
