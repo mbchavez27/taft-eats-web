b7837ec5c0e008c0ec98cd630938f43f2b8970c1
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2026-02-09 14:48:01 +0800

    feat: add owner settings

--- client/app/features/users/containers/owner-settings.tsx
+++ client/app/features/users/containers/owner-settings.tsx
@@ -16,8 +16,25 @@ export default function OwnerSettings() {
           </div>
         </PopoverTrigger>
         <PopoverContent className="bg-white text-[#326F33] border-2 border-[#326F33] font-lexend font-bold px-6 py-8 flex flex-col justify-center items-center gap-6">
-          <PopoverHeader className="flex flex-col justify-center items-center gap-2"></PopoverHeader>
-          <section className="font-inter font-semibold text-xl flex flex-col gap-4"></section>
+          <PopoverHeader className="flex flex-col justify-center items-center gap-2">
+            <h1 className="text-3xl font-lexend font-bold">Settings</h1>
+          </PopoverHeader>
+          <section className="font-inter font-semibold text-md flex flex-col gap-4">
+            <button className="px-4 py-2 text-black border-black border rounded-2xl hover:opacity-50 transition duration-100">
+              Edit Establishment Banner
+            </button>
+            <button className="px-4 py-2 text-black border-black border rounded-2xl hover:opacity-50 transition duration-100">
+              Mark "Temporarily Closed"
+            </button>
+            <div className="flex flex-col gap-3 font-semibold items-center text-center">
+              <p className="text-xs text-[#ED1C24]">
+                Warning: this action cannot be undone!
+              </p>
+              <button className="px-4 py-2 text-white bg-[#326F33] border-[#326F33] border rounded-2xl hover:opacity-50 duration-100 transition">
+                Delete Establishment
+              </button>
+            </div>
+          </section>
         </PopoverContent>
       </Popover>
     </>
