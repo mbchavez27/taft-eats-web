f51754ff55c45d4713ebc4b8a1d64978e92cc13b
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2026-02-13 01:06:10 +0900

    feat: made it mobile responsive

--- client/app/routes/auth/sign-up/index.tsx
+++ client/app/routes/auth/sign-up/index.tsx
@@ -2,54 +2,51 @@ import { Checkbox } from "~/components/ui/checkbox";
 import type { Route } from "./+types/index";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "Taft Eats" },
-    { name: "description", content: "Taft Eats" },
-  ];
+  return [{ title: "Sign Up" }, { name: "description", content: "Taft Eats" }];
 }
 
 export default function SignUp() {
   return (
-    <main className="flex justify-center items-center min-h-screen p-4 bg-gray-50">
+    <main className="flex justify-center items-center min-h-screen p-4">
       <section
         className="
         bg-white 
         font-lexend 
-        flex flex-col 
-        justify-center 
+        flex flex-col justify-center items-center 
         gap-5 
-        p-6 md:p-8 
-        w-full max-w-[626px] 
-        h-auto 
-        rounded-2xl
+        py-8 px-6 
+        w-full max-w-[500px] 
+        rounded-2xl 
         shadow-sm"
       >
-        <div className="mb-2 flex flex-col items-center justify-center">
+        <div className="text-[#326F33] font-bold flex flex-col justify-center items-center text-center">
           <img
             src="/logos/tafteats_logo.png"
             alt="Taft Eats Logo"
-            className="mx-auto mb-4 w-[120px] h-[120px] md:w-[150px] md:h-[150px] object-contain"
+            className="w-[150px] h-[150px] object-contain"
           />
-          <h1 className="text-center text-2xl md:text-3xl text-[#326F33] font-bold mb-2 leading-tight">
+
+          <h1 className="text-2xl md:text-3xl font-bold mt-2 leading-tight">
             Sign Up to unlock the <br className="hidden md:block" /> best of
             Taft Eats!
           </h1>
         </div>
 
-        <div className="flex flex-col w-full">
-          <label htmlFor="name" className="font-medium mb-1">
+        <div className="flex flex-col">
+          <label htmlFor="name" className="text-black font-medium mb-1">
             Name *
           </label>
           <input
             type="text"
             id="name"
             name="name"
+            placeholder="Enter your full name"
             className="border-2 border-black rounded-md w-full p-2 outline-none focus:border-[#326F33]"
             required
           />
         </div>
 
-        <div className="flex flex-col w-full">
+        <div className="flex flex-col">
           <label htmlFor="email" className="text-black font-medium mb-1">
             Email *
           </label>
@@ -57,38 +54,44 @@ export default function SignUp() {
             type="email"
             id="email"
             name="email"
+            placeholder="Enter your email"
             className="border-2 border-black rounded-md w-full p-2 outline-none focus:border-[#326F33]"
             required
           />
         </div>
 
-        <div className="flex flex-col w-full">
-          <label htmlFor="password" className="font-medium mb-1">
+        <div className="flex flex-col">
+          <label htmlFor="password" className="text-black font-medium mb-1">
             Password *
           </label>
           <input
             type="password"
             id="password"
             name="password"
+            placeholder="Create a password"
             className="border-2 border-black rounded-md w-full p-2 outline-none focus:border-[#326F33]"
             required
           />
         </div>
 
-        <div className="flex flex-col w-full">
-          <label htmlFor="confirm-password" className="font-medium mb-1">
+        <div className="flex flex-col">
+          <label
+            htmlFor="confirm-password"
+            className="text-black font-medium mb-1"
+          >
             Confirm Password *
           </label>
           <input
             type="password"
             id="confirm-password"
             name="confirm-password"
+            placeholder="Confirm your password"
             className="border-2 border-black rounded-md w-full p-2 outline-none focus:border-[#326F33]"
             required
           />
         </div>
 
-        <div className="flex items-start gap-2 text-[#9CB16F] w-full mt-1">
+        <div className="flex items-start justify-center gap-2 text-[#9CB16F] w-full mt-1">
           <Checkbox
             id="terms"
             name="terms"
@@ -114,16 +117,18 @@ export default function SignUp() {
           </label>
         </div>
 
-        <div className="flex flex-col mt-2">
+        <div className="flex">
           <button
             type="submit"
-            value="Next"
             className="
               bg-[#326F33] 
               text-white 
               font-bold
-              py-3 
-              rounded-md
+              rounded-lg
+              w-full 
+              py-2 
+              px-6
+              cursor-pointer 
               hover:bg-[#285a29] 
               transition-colors
             "
