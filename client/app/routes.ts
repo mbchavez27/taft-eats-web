13b1f2c8be13289a6afda4bace234a090c7bb7cf
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2026-02-13 01:32:03 +0900

    hotfix: fix the login

--- client/app/routes.ts
+++ client/app/routes.ts
@@ -4,23 +4,28 @@ import {
   index,
   prefix,
   layout,
-} from '@react-router/dev/routes'
+} from "@react-router/dev/routes";
 
 export default [
-  layout('./routes/layout.tsx', [
-    index('./routes/index.tsx'),
+  layout("./routes/layout.tsx", [
+    index("./routes/index.tsx"),
 
-    ...prefix('restaurants', [
-      route('owner', './routes/restaurants/owner/index.tsx'),
-      route(':restaurant', './routes/restaurants/index.tsx'),
+    ...prefix("restaurants", [
+      route("owner", "./routes/restaurants/owner/index.tsx"),
+      route(":restaurant", "./routes/restaurants/index.tsx"),
     ]),
 
-    ...prefix('user', [route('/', './routes/user-page/index.tsx')]),
+    ...prefix("user", [route("/", "./routes/user-page/index.tsx")]),
 
-    ...prefix('maps', [route('/', './routes/map-view/index.tsx')]),
+    ...prefix("maps", [route("/", "./routes/map-view/index.tsx")]),
+
+    ...prefix("auth", [
+      route("login", "./routes/auth/login/index.tsx"),
+      route("sign-up", "./routes/auth/sign-up/index.tsx"),
+    ]),
   ]),
 
-  ...prefix('admin', [
-    layout('./routes/admin/layout.tsx', [index('./routes/admin/index.tsx')]),
+  ...prefix("admin", [
+    layout("./routes/admin/layout.tsx", [index("./routes/admin/index.tsx")]),
   ]),
-] satisfies RouteConfig
+] satisfies RouteConfig;
