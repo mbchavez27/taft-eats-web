33890c6f188b71b238216ab28845cbb538529857
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2026-02-13 12:38:34 +0900

    hotfix: fix the owner link

--- client/app/features/reviews/containers/reviews-list.tsx
+++ client/app/features/reviews/containers/reviews-list.tsx
@@ -1,21 +1,22 @@
-import { ScrollArea } from '~/components/ui/scroll-area'
-import SingleReview from '../components/organisms/single-review'
+import { useLocation } from "react-router";
+import { ScrollArea } from "~/components/ui/scroll-area";
+import SingleReview from "../components/organisms/single-review";
 
 export default function ReviewsList() {
+  const location = useLocation();
+
   return (
     <main className="bg-white rounded-xl w-full p-4 sm:p-6 flex flex-col gap-4 h-125 overflow-hidden">
       <ScrollArea className="h-full w-full overflow-x-auto">
         <div className="flex flex-col gap-4">
-          <SingleReview />
-          <SingleReview />
-          <SingleReview />
-          <SingleReview />
-          <SingleReview />
-          <SingleReview />
-          <SingleReview />
-          <SingleReview />
+          {Array.from({ length: 8 }).map((_, index) => (
+            <SingleReview
+              key={index}
+              is_owner={location.pathname?.includes("restaurants/owner")}
+            />
+          ))}
         </div>
       </ScrollArea>
     </main>
-  )
+  );
 }
